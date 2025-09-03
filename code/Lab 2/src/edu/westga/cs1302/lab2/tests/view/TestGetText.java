c259ae86f8f33e2a1f2a8c17ea28f5b2230356c7
ever dewb <everetttdewberry@gmail.com>
2025-09-03 11:17:22 -0400

    Added TestGetText to tests/view.
    Sets up a bill and adds a single item (cheese).
    
    Asserts that the text returned by getText is the same as expected text.

---
+++ code/Lab 2/src/edu/westga/cs1302/lab2/tests/view/TestGetText.java
@@ -0,0 +1,28 @@
+package edu.westga.cs1302.lab2.tests.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+import edu.westga.cs1302.lab2.view.BillView;
+
+class TestGetText {
+
+	@Test
+	void testGetText() {
+		Bill bill = new Bill();
+		BillItem cheese = new BillItem("Cheese", 1.00);
+		bill.addItem(cheese);
+		BillView view = new BillView();
+		assertEquals("ITEMS\r\n"
+				+ "Cheese - 1.0\r\n"
+				+ "\r\n"
+				+ "SUBTOTAL - $1.0\r\n"
+				+ "TAX - $0.1\r\n"
+				+ "TIP - $0.2\r\n"
+				+ "TOTAL - $1.3", view.getText(bill));
+	}
+
+}
