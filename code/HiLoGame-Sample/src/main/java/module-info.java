c79cd84d04ca2d306e1f08d6b4abbc23e369bf36
ever dewb <everetttdewberry@gmail.com>
2025-09-04 09:34:34 -0400

    Removed old HiLoGame-Sample/
    ^

--- code/HiLoGame-Sample/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.westga.cs1302.hilogame {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens edu.westga.cs1302.hilogame.views to javafx.fxml;
-    exports edu.westga.cs1302.hilogame;
-}
