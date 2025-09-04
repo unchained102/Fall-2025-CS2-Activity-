8f754926f6f071d90175957e4b057e03a9ec135f
ever dewb <everetttdewberry@gmail.com>
2025-09-04 12:47:58 -0400

    Completed displaying bill.
    Displays the bill using JavaFX.
    Allows adding an arbitrary number of items.

--- code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,11 +1,12 @@
 package edu.westga.cs1302.lab3.views;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import edu.westga.cs1302.lab3.model.*;
-import edu.westga.cs1302.lab3.views.BillView;
 
 
 /**
@@ -20,20 +21,45 @@ public class MainWindow {
     private TextArea output;
 
     @FXML
-    private TextField input;
+    private TextField inputName;
+    
+    @FXML
+    private TextField inputAmount;
+    
+    @FXML
+    private Button button;
+
+	private String name;
+
+	private String sAmount;
+
+	private Double dAmount;
+	
+	private BillItem item;
+	
+	private Bill bill;
+	
+	private BillView view;
     
     
     @FXML
     void displayText(ActionEvent event) {
-    	String input = this.input.getText();
-    	this.output.setText(input);
+    	this.name = this.inputName.getText();
+    	this.sAmount = this.inputAmount.getText();
+    	this.dAmount = Double.parseDouble(this.sAmount);
+    	this.item = new BillItem(this.name, this.dAmount);
+    	this.bill.addItem(item);
+    	this.output.setText(this.view.getText(this.bill));
+    	
     }
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
-    	
+    	Platform.runLater( () -> button.requestFocus() );
+    	this.bill = new Bill();
+    	this.view = new BillView();
     }
     
     
