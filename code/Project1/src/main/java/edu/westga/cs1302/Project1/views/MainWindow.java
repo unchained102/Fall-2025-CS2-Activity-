a90682d85517c302c06efb3c182aca0c69bbfdbb
ever dewb <overwatchad2000@gmail.com>
2025-09-22 09:52:43 -0400

    Created base page layout.
    MainWindow.fxml has been edited to include all specified components.
    Label and field for name and description, label and combo-box for priority,
    a submit button, and a listview for viewing stored tasks.
    
    MainWindow.java has also been updated to include relevant methods and fields for the above components.

--- code/Project1/src/main/java/edu/westga/cs1302/Project1/views/MainWindow.java
+++ code/Project1/src/main/java/edu/westga/cs1302/Project1/views/MainWindow.java
@@ -1,5 +1,11 @@
 package edu.westga.cs1302.Project1.views;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,6 +14,25 @@ package edu.westga.cs1302.Project1.views;
  * @version Fall 2025
  */
 public class MainWindow {
+    @FXML
+    private TextArea descriptionBox;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private ComboBox<?> priorityBox;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private ListView<?> taskList;
+    
+    @FXML
+    void submitPressed(ActionEvent event) {
+
+    }
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
