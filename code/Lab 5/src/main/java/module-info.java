08a64df9405e0541988edf6ec0e5c4286cbd1425
ever dewb <overwatchad2000@gmail.com>
2025-09-25 11:56:23 -0400

    Removed Lab 5 to start over.
    Went off course tremendously.

--- code/Lab 5/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.westga.cs1302.lab5 {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens edu.westga.cs1302.lab5.views to javafx.fxml;
-    exports edu.westga.cs1302.lab5;
-}
