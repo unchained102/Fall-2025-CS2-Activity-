6ecd49964b6b34072099bfcae428e14c4c87f7b2
ever dewb <overwatchad2000@gmail.com>
2025-10-25 09:29:46 -0400

    Removed old stuff.
    I am fairly certain now that what was added last commit WAS my Project 2 starter. Now I have removed it so that I can re-commit it with an appropriately designated message.

--- code/Task Tracker/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.westga.cs1302.task_tracker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-
-    opens edu.westga.cs1302.task_tracker.views to javafx.fxml;
-    exports edu.westga.cs1302.task_tracker;
-}
