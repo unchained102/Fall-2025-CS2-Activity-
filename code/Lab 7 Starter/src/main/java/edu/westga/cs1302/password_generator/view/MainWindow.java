b816b28e67051f7c558415bf6e49eff8723eb044
ever dewb <overwatchad2000@gmail.com>
2025-11-05 11:49:34 -0500

    Lab 7: Integrated Viewmodel with Code Behind.
    The program is functionally identical, but now all of the logic that was previously handled by the MainWindow class, is now handled by the GeneratorViewModel class.
    The only thing MainWindow handles now are UI concerns, like showing alerts when exceptions are thrown, or listening for the button to be pressed.

--- code/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -1,12 +1,10 @@
 package edu.westga.cs1302.password_generator.view;
 
-import java.util.Random;
-
-import edu.westga.cs1302.password_generator.model.PasswordGenerator;
-import javafx.event.ActionEvent;
+import edu.westga.cs1302.password_generator.viewmodel.GeneratorViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -18,45 +16,14 @@ import javafx.scene.control.TextField;
  */
 public class MainWindow {
 
+	@FXML private Button generate;
     @FXML private CheckBox mustIncludeDigits;
     @FXML private CheckBox mustIncludeLowerCaseLetters;
     @FXML private CheckBox mustIncludeUpperCaseLetters;
     @FXML private TextField minimumLength;
     @FXML private TextArea output;
+    private GeneratorViewModel vm;
     
-    private PasswordGenerator generator;
-
-    @FXML
-    void generatePassword(ActionEvent event) {
-    	int minimumLength = -1;
-    	
-    	try {
-    		minimumLength = Integer.parseInt(this.minimumLength.getText());
-    	} catch (NumberFormatException numberError) {
-    		Alert alert = new Alert(AlertType.ERROR);
-    		alert.setContentText("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getText());
-    		alert.show();
-    		return;
-    	}
-    	
-    	try {
-    		this.generator.setMinimumLength(minimumLength);
-    	} catch (IllegalArgumentException invalidLengthError) {
-    		Alert alert = new Alert(AlertType.ERROR);
-    		alert.setContentText("Invalid Minimum Length: " + invalidLengthError.getMessage());
-    		alert.show();
-    		return;
-    	}
-    	
-    	this.generator.setMustHaveAtLeastOneDigit(this.mustIncludeDigits.isSelected());
-    	this.generator.setMustHaveAtLeastOneLowerCaseLetter(this.mustIncludeLowerCaseLetters.isSelected());
-    	this.generator.setMustHaveAtLeastOneUpperCaseLetter(this.mustIncludeUpperCaseLetters.isSelected());
-    	
-    	String password = this.generator.generatePassword();
-    	
-    	this.output.setText(password);
-    }
-
     @FXML
     void initialize() {
         assert this.mustIncludeDigits != null : "fx:id=\"mustIncludeDigits\" was not injected: check your FXML file 'MainWindow.fxml'.";
@@ -65,8 +32,26 @@ public class MainWindow {
         assert this.minimumLength != null : "fx:id=\"minimumLength\" was not injected: check your FXML file 'MainWindow.fxml'.";
         assert this.output != null : "fx:id=\"output\" was not injected: check your FXML file 'MainWindow.fxml'.";
 
-        this.minimumLength.setText("1");
-        Random randomNumberGenerator = new Random();
-        this.generator = new PasswordGenerator(randomNumberGenerator.nextLong());
+        this.vm = new GeneratorViewModel();
+        this.minimumLength.textProperty().bindBidirectional(this.vm.getMinimumLengthProperty());
+        this.mustIncludeDigits.selectedProperty().bindBidirectional(this.vm.getMustIncludeDigitsProperty());
+        this.mustIncludeLowerCaseLetters.selectedProperty().bindBidirectional(this.vm.getMustIncludeLowerCaseLettersProperty());
+        this.mustIncludeUpperCaseLetters.selectedProperty().bindBidirectional(this.vm.getMustIncludeUpperCaseLettersProperty());
+        this.output.textProperty().bind(this.vm.getOutputProperty());
+        this.generate.setOnAction((event) -> {
+        	try {
+        		this.vm.generatePassword();
+        	} catch (NumberFormatException numberError) {
+        		Alert alert = new Alert(AlertType.ERROR);
+        		alert.setContentText("Invalid Minimum Length: must be a positive integer, but was " + this.minimumLength.getText());
+        		alert.show();
+        		return;
+        	 } catch (IllegalArgumentException invalidLengthError) {
+        		Alert alert = new Alert(AlertType.ERROR);
+        		alert.setContentText("Invalid Minimum Length: " + invalidLengthError.getMessage());
+        		alert.show();
+        		return;
+        	}
+        });
     }
 }
