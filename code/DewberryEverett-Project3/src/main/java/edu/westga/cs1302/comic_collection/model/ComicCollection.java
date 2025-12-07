00d2bdc905c38b37863a30d399ee0a9f42e35e1b
ever dewb <overwatchad2000@gmail.com>
2025-12-06 19:15:50 -0500

    Project 3 1-B: Added ComicCollection dataclass.
    Barebones ComicCollection dataclass. Currently has a name, a constructor, and a getter for the name.

---
+++ code/DewberryEverett-Project3/src/main/java/edu/westga/cs1302/comic_collection/model/ComicCollection.java
@@ -0,0 +1,33 @@
+package edu.westga.cs1302.comic_collection.model;
+
+/** Represents a comid collection
+ * 
+ * @author Me
+ * @version 1.0
+ */
+public class ComicCollection {
+	private String name;
+	
+	/** Creates a new ComicCollection with the given name.
+	 * 
+	 * @param name the name of the ComicCollection
+	 */
+	public ComicCollection(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("Name cannot be null.");
+		}
+		if (name.equals("")) {
+			throw new IllegalArgumentException("Name cannot be empty.");
+		}
+		
+		this.name = name;
+	}
+	
+	/** Name getter
+	 * 
+	 * @return String name
+	 */
+	public String getName() {
+		return this.name;
+	}
+}
